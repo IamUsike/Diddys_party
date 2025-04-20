6eb58ecf6097ab58b5bb47f632d932a27837fa1a
IamUsike <sathwikthanmay4@gmail.com>
2025-04-20 07:35:08 +0530

    some cors shi

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.CORS_ORIGIN, "http://127.0.0.1:5555"],
+    origin: [process.env.CORS_ORIGIN, "http://127.0.0.1:3000"],
     credentials: true,
   }),
 );
